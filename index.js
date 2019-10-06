a2015d6ddf884e6788a9d3df6c4e1d994b42ec09
Jay Li <jayli3n@gmail.com>
2019-10-06 22:37:59 +0800

    Added npm script start

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { handleRequest } = require('./server');
 
 // Initialize express.
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
 	console.log('Server is running on port: ', PORT);
 });
